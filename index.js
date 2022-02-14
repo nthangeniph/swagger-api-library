f0220f9545bcf7f5772644da24e9f5afe54ded5f
nthangeniphumudzo <60381173+nthangeniphumudzo@users.noreply.github.com>
2022-02-15 00:50:37 +0200

    url change

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ const db = low(adapter);
 db.defaults({ books: [] }).write();
 
 const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Library API",
-			version: "1.0.0",
-			description: "A simple Express Library API",
-		},
-		servers: [
-			{
-				url: "http://localhost:4000",
-			},
-		],
-	},
-	apis: ["./routes/*.js"],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "https://airtablegenerator.azurewebsites.net/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
 };
 
 const specs = swaggerJsDoc(options);
