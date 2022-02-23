e9bedb72d67db7b227a15b0c6242ba71caa875cc
nthangeniphumudzo <60381173+nthangeniphumudzo@users.noreply.github.com>
2022-02-23 19:20:14 +0200

    add more crud

--- routes/books.js
+++ routes/books.js
@@ -93,28 +93,28 @@ router.get("/:id", (req, res) => {
   res.send(book);
 });
 
-// /**
-//  * @swagger
-//  * /books:
-//  *   post:
-//  *     summary: Create a new book
-//  *     tags: [Books]
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             $ref: '#/components/schemas/Book'
-//  *     responses:
-//  *       200:
-//  *         description: The book was successfully created
-//  *         content:
-//  *           application/json:
-//  *             schema:
-//  *               $ref: '#/components/schemas/Book'
-//  *       500:
-//  *         description: Some server error
-//  */
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The book was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ */
 
 router.post("/", (req, res) => {
   try {
@@ -131,37 +131,37 @@ router.post("/", (req, res) => {
   }
 });
 
-// /**
-//  * @swagger
-//  * /books/{id}:
-//  *  put:
-//  *    summary: Update the book by the id
-//  *    tags: [Books]
-//  *    parameters:
-//  *      - in: path
-//  *        name: id
-//  *        schema:
-//  *          type: string
-//  *        required: true
-//  *        description: The book id
-//  *    requestBody:
-//  *      required: true
-//  *      content:
-//  *        application/json:
-//  *          schema:
-//  *            $ref: '#/components/schemas/Book'
-//  *    responses:
-//  *      200:
-//  *        description: The book was updated
-//  *        content:
-//  *          application/json:
-//  *            schema:
-//  *              $ref: '#/components/schemas/Book'
-//  *      404:
-//  *        description: The book was not found
-//  *      500:
-//  *        description: Some error happened
-//  */
+/**
+ * @swagger
+ * /books/{id}:
+ *  put:
+ *    summary: Update the book by the id
+ *    tags: [Books]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The book id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Book'
+ *    responses:
+ *      200:
+ *        description: The book was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Book'
+ *      404:
+ *        description: The book was not found
+ *      500:
+ *        description: Some error happened
+ */
 
 router.put("/:id", (req, res) => {
   try {
@@ -173,26 +173,26 @@ router.put("/:id", (req, res) => {
   }
 });
 
-// /**
-//  * @swagger
-//  * /books/{id}:
-//  *   delete:
-//  *     summary: Remove the book by id
-//  *     tags: [Books]
-//  *     parameters:
-//  *       - in: path
-//  *         name: id
-//  *         schema:
-//  *           type: string
-//  *         required: true
-//  *         description: The book id
-//  *
-//  *     responses:
-//  *       200:
-//  *         description: The book was deleted
-//  *       404:
-//  *         description: The book was not found
-//  */
+/**
+ * @swagger
+ * /books/{id}:
+ *   delete:
+ *     summary: Remove the book by id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *
+ *     responses:
+ *       200:
+ *         description: The book was deleted
+ *       404:
+ *         description: The book was not found
+ */
 
 router.delete("/:id", (req, res) => {
   req.app.db.get("books").remove({ id: req.params.id }).write();
